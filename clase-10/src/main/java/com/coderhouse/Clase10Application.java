441b7e342348c805c12f80ffb341fe93a65cd78d
Gerardo <geridepetris94@gmail.com>
2024-12-22 15:50:40 -0300

    arreglo

--- clase-10/src/main/java/com/coderhouse/Clase10Application.java
+++ clase-10/src/main/java/com/coderhouse/Clase10Application.java
@@ -12,12 +12,6 @@ public class Clase10Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Clase10Application.class, args);
 	}
-<<<<<<< HEAD
-		saxvcz
-=======
 	
-	
-	
->>>>>>> 21b745bdc1402c8b38c4e767ef92cc66327ceeaa
 	
 }
